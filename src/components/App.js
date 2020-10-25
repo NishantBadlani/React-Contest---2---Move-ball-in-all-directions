262db27c2c871cc42d815b5f6e2e2044eeaddb72
NishantBadlani <nishant.03badlani@gmail.com>
2020-10-25 20:23:50 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,52 @@
-import React, { Component, useState } from "react";
+import React, {Component, useState, useEffect} from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      if (event.keyCode === 37) {
+        setX(x - 5);
+      } else if (event.keyCode === 38) {
+        setY(y - 5);
+      } else if (event.keyCode === 39) {
+        setX(x + 5);
+      } else if (event.keyCode === 40) {
+        setY(y + 5);
+      }
+      setBallPosition({
+        left: `${x}px`,
+        top: `${y}px`
+      });
+    });
+  });
+
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({
+      left: "0px",
+      top: "0px"
+    });
+  };
+
+  const buttonClickHandler = () => {
+    setRenderBall(!renderBall);
+  };
+
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else return <button onClick={buttonClickHandler}>Click For One Ball</button>;
+  };
 
   return (
     <div className="playground">
